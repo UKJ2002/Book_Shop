2e66a86196a92c7396f0e1ad85fe812774487012
UKJ2002 <S23009752@ousl.lk>
2024-11-10 11:50:37 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,5 @@
+function toggleCategoryList() {
+    const categoryList = document.getElementById("categoryList");
+    // Toggle display between 'none' and 'block'
+    categoryList.style.display = categoryList.style.display === "none" ? "block" : "none";
+}
